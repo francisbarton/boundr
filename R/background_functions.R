99b236472976785d4a7a51765e1e10d3fe148b48
Fran Barton <fbarton@alwaysdata.net>
2022-07-31 22:26:29 +0100

    reorganisation

--- R/background_functions.R
+++ R/background_functions.R
@@ -1,141 +1,6 @@
-pull_geo_query_url <- function(geo_code_field, resolution, geo_option = 1) {
-
-  # codes <- opengeo_schema %>%
-  #   dplyr::filter(!stringr::str_detect(service_name, "_LU$")) %>%
-  #   janitor::remove_empty("cols") %>%
-  #   dplyr::select(ends_with("cd")) %>%
-  #   janitor::remove_empty("rows") %>%
-  #   names()
-  #
-  # x <- test_prefixes(codes, x)
-  # x_code <- find_codes(codes, x, year)
-  #
-  # usethis::ui_info(
-  #   stringr::str_glue("Using code {x_code}.")
-  # )
-
-  results <- opengeo_schema %>%
-    dplyr::arrange(desc(edit_date)) %>%
-    dplyr::filter(!stringr::str_detect(service_name, "_LU$")) %>%
-    dplyr::filter(stringr::str_detect(service_name, toupper(resolution))) %>%
-    dplyr::filter(!is.na(geo_code_field)) %>%
-    janitor::remove_empty("cols")
-
-  msg <- stringr::str_wrap(
-    "No result was found for the parameters supplied. Try a different year or a different resolution?", 72)
-  assertthat::assert_that(nrow(results) > 0, msg = msg)
-
-  if (nrow(results) > 1) {
-    usethis::ui_info(
-      stringr::str_c(
-        "More than 1 result found:",
-        stringr::str_c(
-          paste0(
-            "\t(",
-            seq(nrow(results)),
-            ") ",
-            results$service_name),
-          collapse = "\n"),
-        "Using option {geo_option}. (Change the `geo_option` parameter to use a different one.)", sep = "\n"))
-  }
-
-  results %>%
-    dplyr::slice(geo_option) %>%
-    dplyr::select(service_url, x_code = geo_code_field) %>%
-    as.list()
-}
 
 
-pull_lookup_query_url <- function(x, y, year_x = NULL, year_y = NULL, country_filter = c("UK|EW|EN|WA", "UK", "EW", "EN", "WA"), option = 1) {
-
-  countries <- match.arg(country_filter)
-
-  # filter only lookup tables from the schema
-  schema_lookups <- opengeo_schema %>%
-    dplyr::filter(stringr::str_detect(service_name, "_LU$")) %>%
-    janitor::remove_empty("cols")
-
-  # make list of codes used in lookups
-  schema1_names <- schema_lookups %>%
-    dplyr::select(ends_with("cd")) %>%
-    names()
-
-  # make sure that x matches one of the
-  x <- test_prefixes(schema1_names, x)
-  x_code <- find_codes(schema1_names, x, year_x)
-
-  schema2 <- schema1 %>%
-    dplyr::filter(!across(x_code, is.na)) %>%
-    janitor::remove_empty("cols")
-
-  codes2 <- schema2 %>%
-    dplyr::select(ends_with("cd")) %>%
-    names()
-
-  y <- test_prefixes(codes2, y)
-  y_code <- find_codes(codes2, y, year_y)
-
-  usethis::ui_info(
-    stringr::str_glue("Using codes {x_code}, {y_code}.")
-  )
-
-  results <- schema2 %>%
-    dplyr::filter(!across(y_code, is.na)) %>%
-    dplyr::filter(stringr::str_detect(service_name, countries)) %>%
-    dplyr::arrange(desc(edit_date))
-
-  msg <- "No result was found for the parameters supplied. Try a different year or a different country filter?"
-  assertthat::assert_that(nrow(results) > 0, msg = msg)
-
-  if (nrow(results) > 1) {
-    usethis::ui_info(
-      stringr::str_c(
-        "More than 1 result found:",
-        stringr::str_c(
-          paste0(
-            "\t(",
-            seq(nrow(results)),
-            ") ",
-            results$service_name),
-          collapse = "\n"),
-        "Using option {option}. (Change the `option` parameter to use a different one.)", sep = "\n"))
-  }
-
-  results %>%
-    dplyr::slice(option) %>%
-    dplyr::select(service_url, x_code = x_code, y_code = y_code) %>%
-    as.list()
-}
-
-
-
-
-
-test_prefixes <- function(schema_names, prefix) {
-
-  # create list of all available prefixes from the schema column names
-  poss_prefixes <- unique(sub("(^[a-z]+)(.*)", "\\1", schema_names))
-
-
-  if (prefix %in% poss_prefixes) prefix
-  else {
-    new_prefix <- grep(paste0("^", prefix), poss_prefixes, perl = TRUE, value = TRUE)[1]
-    new_prefix <- stringr::str_match(poss_prefixes, paste0("^", prefix))
-    if (nzchar(new_prefix)) {
-      stringr::str_glue("Prefix not found. Using {new_prefix} instead.") %>%
-        usethis::ui_info()
-      new_prefix
-    } else {
-      stringr::str_glue("Prefix {prefix} not found.") %>%
-        usethis::ui_stop()
-    }
-  }
-}
-
-
-
-
-find_codes <- function(codes, prefix, year = NULL, cutoff = 30) {
+find_codes <- function(schema, prefix, year = NULL, cutoff = 30) {
 
   out <- NULL
 
@@ -180,3 +45,157 @@ find_codes <- function(codes, prefix, year = NULL, cutoff = 30) {
   # return
   out
 }
+
+
+
+
+
+
+pull_geo_query_url <- function(geo_code_field, resolution, geo_option = 1) {
+
+  # codes <- opengeo_schema %>%
+  #   dplyr::filter(!stringr::str_detect(service_name, "_LU$")) %>%
+  #   janitor::remove_empty("cols") %>%
+  #   dplyr::select(ends_with("cd")) %>%
+  #   janitor::remove_empty("rows") %>%
+  #   names()
+  #
+  # x <- test_prefixes(codes, x)
+  # x_code <- find_codes(codes, x, year)
+  #
+  # usethis::ui_info(
+  #   stringr::str_glue("Using code {x_code}.")
+  # )
+
+  results <- opengeo_schema %>%
+    dplyr::arrange(desc(edit_date)) %>%
+    dplyr::filter(!stringr::str_detect(service_name, "_LU$")) %>%
+    dplyr::filter(stringr::str_detect(service_name, toupper(resolution))) %>%
+    dplyr::filter(!is.na(geo_code_field)) %>%
+    janitor::remove_empty("cols")
+
+  msg <- "No result was found for the parameters supplied. Try a different year or a different resolution?"  %>% 
+    stringr::str_wrap(72)
+  assertthat::assert_that(nrow(results) > 0, msg = msg)
+
+  if (nrow(results) > 1) {
+    usethis::ui_info(
+      stringr::str_c(
+        "More than 1 result found:",
+        stringr::str_c(
+          paste0(
+            "\t(",
+            seq(nrow(results)),
+            ") ",
+            results$service_name),
+          collapse = "\n"),
+        "Using option {geo_option}. (Change the `geo_option` parameter to use a different one.)", sep = "\n"))
+  }
+
+  results %>%
+    dplyr::slice(geo_option) %>%
+    dplyr::select(service_url, x_code = geo_code_field) %>%
+    as.list()
+}
+
+
+pull_lookup_query_url <- function(x, y, year_x = NULL, year_y = NULL, country_filter = c("UK|EW|EN|WA", "UK", "EW", "EN", "WA"), option = 1) {
+
+  countries <- match.arg(country_filter)
+
+  # filter only lookup tables from the schema
+  schema_lookups <- opengeo_schema %>%
+    dplyr::filter(stringr::str_detect(service_name, "_LU$")) %>%
+    janitor::remove_empty("cols")
+
+  # make list of codes used in lookups
+  schema1_names <- schema_lookups %>%
+    dplyr::select(ends_with("cd")) %>%
+    names()
+
+  # make sure that x matches one of the available columns
+  # is check_prefix() really necessary? Can we just do find_codes?
+  x <- check_prefix(schema1_names, x)
+  x_code <- find_codes(schema1_names, x, year_x)
+
+  # reduce schema to only those matching x_code
+  schema2 <- schema1 %>%
+    dplyr::filter(!across(x_code, is.na)) %>%
+    janitor::remove_empty("cols")
+
+  codes2 <- schema2 %>%
+    dplyr::select(ends_with("cd")) %>%
+    names()
+
+  y <- test_prefixes(codes2, y)
+  y_code <- find_codes(codes2, y, year_y)
+
+  usethis::ui_info(
+    stringr::str_glue("Using codes {x_code}, {y_code}.")
+  )
+
+  results <- schema2 %>%
+    dplyr::filter(!across(y_code, is.na)) %>%
+    dplyr::filter(stringr::str_detect(service_name, countries)) %>%
+    dplyr::arrange(desc(edit_date))
+
+  msg <- "No result was found for the parameters supplied. Try a different year or a different country filter?"
+  assertthat::assert_that(nrow(results) > 0, msg = msg)
+
+  if (nrow(results) > 1) {
+    usethis::ui_info(
+      stringr::str_c(
+        "More than 1 result found:",
+        stringr::str_c(
+          paste0(
+            "\t(",
+            seq(nrow(results)),
+            ") ",
+            results$service_name),
+          collapse = "\n"),
+        "Using option {option}. (Change the `option` parameter to use a different one.)", sep = "\n"))
+  }
+
+  results %>%
+    dplyr::slice(option) %>%
+    dplyr::select(service_url, x_code = x_code, y_code = y_code) %>%
+    as.list()
+}
+
+
+query_opengeo_api <- function(url, append = "0") {
+  url %>%
+    httr2::request() %>%
+    httr2::req_headers(UserAgent = "boundr R package") %>%
+    httr2::req_url_path_append(append) %>%
+    httr2::req_url_query(f = "pjson") %>%
+    httr2::req_retry(max_tries = 3)
+}
+
+# safely_query_opengeo_api <- purrr::safely(query_opengeo_api)
+safely_query_opengeo_api <- function(...) "dummy"
+
+
+
+
+# check_prefix <- function(schema_names, prefix) {
+
+#   # create list of all available prefixes from the schema column names
+#   # poss_prefixes <- unique(sub("(^[a-z]+)(.*)", "\\1", schema_names))
+#   poss_prefixes <- unique(stringr::str_extract_all(schema_names, "^[a-z]+"))
+
+
+#   if (prefix %in% poss_prefixes) prefix
+#   else {
+#     # new_prefix <- grep(paste0("^", prefix), poss_prefixes, perl = TRUE, value = TRUE)[1]
+#     new_prefix <- stringr::str_subset(poss_prefixes, paste0("^", prefix))[[1]]
+#     if (nzchar(new_prefix)) {
+#       stringr::str_glue("Prefix not found. Using {new_prefix} instead.") %>%
+#         usethis::ui_info()
+#       new_prefix
+#     } else {
+#       stringr::str_glue("Prefix {prefix} not found.") %>%
+#         usethis::ui_stop()
+#     }
+#   }
+# }
--- data-raw/datasets.R
+++ data-raw/datasets.R
@@ -1,8 +1,72 @@
+# House of Commons Library MSOA Names data -------------
+
+
 hocl_msoa_names <- paste0(
   "https://houseofcommonslibrary.github.io/",
   "msoanames/MSOA-Names-Latest.csv") |>
   readr::read_csv(col_types = "cccccc") |>
   dplyr::select(!Laname)
+
+
+# Open Geography schema build ------------- 
+
+pull_fields <- function(json, url) {
+  json %>% {
+    dplyr::tibble(
+      service_name = purrr::pluck(., "name"),
+      service_type = purrr::pluck(., "type"),
+      version = purrr::pluck(., "currentVersion"),
+      edit_date = purrr::pluck(., "editingInfo", "lastEditDate"),
+      has_geometry = purrr::pluck(., "hasGeometryProperties", .default = FALSE),
+      fields = purrr::pluck(., "fields", .default = list(name = NULL))
+    )
+  } %>%
+    tidyr::hoist(fields, "name") %>%
+    dplyr::select(1:name) %>%
+    tidyr::pivot_wider(names_from = name, values_from = name) %>%
+    janitor::clean_names() %>%
+    dplyr::mutate(service_url = url, .before = 1) %>%
+    dplyr::select(service_url:has_geometry, ends_with("cd")) %>%
+    dplyr::filter(!if_all(ends_with("cd"), is.na))
+}
+
+# build_schema() ------------- 
+
+build_schema <- function() {
+  server_url <- paste0(
+    "https://services1.arcgis.com/ESMARspQHYMw9BZ9/",
+    "ArcGIS/rest/services")
+
+  urls <- server_url %>%
+    safely_query_opengeo_api(append = "") %>%
+    purrr::pluck("result") %>% 
+    httr2::resp_body_json() %>%
+    purrr::pluck("services") %>%
+    purrr::map_chr("url")
+
+  responses <- urls %>%
+    purrr::map(safely_query_opengeo_api)
+
+  responses %>%
+    purrr::map("error") %>%
+    purrr::compact() %>%
+    purrr::map("resp") %>%
+    purrr::walk(httr2::resp_check_status)
+
+  success_numbers <- responses %>%
+    purrr::map("result") %>%
+    purrr::map_lgl(~ !is.null(.)) %>%
+    which()
+
+  success_urls <- urls[success_numbers]
+
+  responses %>%
+    purrr::map("result") %>%
+    purrr::compact() %>%
+    purrr::map(httr2::resp_body_json) %>%
+    purrr::map2_df(success_urls, pull_fields)
+}
+
 
 
 opengeo_schema <- build_schema()
