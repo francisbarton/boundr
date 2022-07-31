676d2589bcda5bef780d89be1adf4bd76feb364f
Fran Barton <fbarton@alwaysdata.net>
2022-07-31 12:22:56 +0100

    Remove old jogger code

--- R/lad21nmw_lookup.R
+++
@@ -1,9 +0,0 @@
-#' A list of all LADs in England and Wales: codes and names
-#'
-#' @format A data frame with 374 rows and 3 variables:
-#' \describe{
-#'   \item{lad21cd}{LAD code}
-#'   \item{lad21nm}{LAD name (English)}
-#'   \item{lad21nmw}{LAD name (Welsh)}
-#' }
-"lad21nmw_lookup"
--- R/lsoa11cdnm.R
+++
@@ -1,8 +0,0 @@
-#' A list of all LSOAs in England and Wales: codes and names
-#'
-#' @format A data frame with 34753 rows and 2 variables:
-#' \describe{
-#'   \item{lsoa11cd}{LSOA code}
-#'   \item{lsoa11nm}{LSOA name}
-#' }
-"lsoa11cdnm"
