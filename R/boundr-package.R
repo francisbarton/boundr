057da5790c689b526a7f1163ed671c1ca200026e
Francis Barton <francis.barton@nuh.nhs.uk>
2023-01-04 02:07:00 +0000

    Little tidy-ups and updates

--- R/boundr-package.R
+++ R/boundr-package.R
@@ -2,11 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom magrittr %>%
 #' @importFrom dplyr any_of all_of contains ends_with starts_with everything
 #' @importFrom dplyr last_col matches num_range one_of across if_any if_all
 #' @importFrom dplyr first last nth
-#' @importFrom assertthat assert_that
 #' @importFrom rlang sym
 
 ## usethis namespace: end
--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onLoad <- function(lib, pkg) {
-  safely_query_opengeo_api <<- purrr::safely(query_opengeo_api)
-}
