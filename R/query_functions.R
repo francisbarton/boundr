c1e4bd77301d00cde98eba67e1d1bd845be9a820
Fran Barton <fbarton@alwaysdata.net>
2023-02-27 01:23:58 +0000

    Improvements to the API query workhorse functions 🩹

--- R/query_functions.R
+++ R/query_functions.R
@@ -2,12 +2,12 @@
 
 
 #' Basic OpenGeography API request
-#' 
+#'
 #' @param url The API URL to query
 #' @param append string to be appended to the URL path
 #' @param format data format to return from the API, `"pjson"` by default
 #' @param user_agent Allows user to specify a certain User Agent string in the
-#'   header of each query. Sends the string `"boundr R package"` by default
+#'  header of each query. Sends the string `"boundr R package"` by default
 opengeo_api_req <- function(
   url,
   append = "0/query",
@@ -23,7 +23,7 @@ opengeo_api_req <- function(
 
 
 #' Build an API request for IDs only
-#' 
+#'
 #' @inheritParams opengeo_api_req
 #' @param where any geographical filters to be applied
 #' @param ... any arguments to be passed to `opengeo_api_req()`
@@ -37,13 +37,14 @@ build_id_req <- function(url, where, ...) {
 
 
 #' Build an API request for table data
-#' 
+#'
 #' @inheritParams opengeo_api_req
 #' @param ids the IDs of the data to be requested
 #' @param fields which fields to include in the table returned
 #' @param ... any arguments to be passed to `opengeo_api_req()`
 table_data_req <- function(ids, url, fields, ...) {
   ids <- stringr::str_flatten(ids, collapse = ",")
+  fields <- stringr::str_flatten(fields, collapse = ",")
   url |>
     opengeo_api_req(...) |>
     httr2::req_url_query(objectIds = ids) |>
@@ -55,7 +56,7 @@ table_data_req <- function(ids, url, fields, ...) {
 
 
 #' Build an API request for spatial boundary (geojson) data
-#' 
+#'
 #' @inheritParams table_data_req
 #' @param crs the Coordinate Reference System (CRS) code to use. 4326 by
 #'  default.
@@ -77,7 +78,7 @@ bounds_data_req <- function(ids, url, crs = 4326, ...) {
 # Perform requests -------------------------------
 
 #' Perform an API query using a request object
-#' 
+#'
 #' @param req An `httr2` request object
 #' @param max_tries integer. 3 by default (for now). Passed to `httr2::req_retry
 #'  ()` and controls how many times to try to perform the request, should
@@ -87,13 +88,16 @@ bounds_data_req <- function(ids, url, crs = 4326, ...) {
 #'  Can be any integer from 0 to 3, or `NULL`. See `?req_perform` for more
 #'  detail.
 query_opengeo_api <- function(req, max_tries = 3, verbosity = 0) {
-  assertthat::assert_that(verbosity %in% 0:3 | is.null(verbosity),
-      msg = "query_opengeo_api: invalid value for verbosity parameter. it ust be an integer between 0 and 3, or NULL.")
-  assertthat::assert_that(is.numeric(max_tries) | is.null(max_tries),
-      msg = "query_opengeo_api: max_tries must be a numeric (integer) value, or NULL.")
+  assert_that(verbosity %in% 0:3 | is.null(verbosity),
+      msg = paste0(
+        "query_opengeo_api: ",
+        "invalid value for `verbosity` parameter. ",
+        "It must be an integer between 0 and 3, or NULL."))
 
-  # just make sure it is an integer
-  if (!is.null(max_tries)) max_tries <- round(max_tries)
+  assert_that(is.numeric(max_tries) | is.null(max_tries),
+      msg = paste0(
+        "query_opengeo_api: ",
+        "`max_tries` must be a numeric (integer) value, or NULL."))
 
   req |>
     httr2::req_retry(max_tries = max_tries) |>
@@ -105,14 +109,17 @@ possibly_query_opengeo_api <- function(...) {
   purrr::possibly(query_opengeo_api)(...)
 }
 
-
+#' @noRd
+possibly_parse_json <- function(...) {
+  purrr::possibly(httr2::resp_body_json)(...)
+}
 
 
 # Response handling ----------------------------
 
 
 #' Perform an API query and extract the returned IDs
-#' 
+#'
 #' For large queries, just return IDs that can then be batched for full queries.
 #' @inheritParams build_id_req
 #' @inheritParams query_opengeo_api
@@ -121,15 +128,21 @@ return_result_ids <- function(url, where, max_tries = 3, verbosity = 0, ...) {
     possibly_query_opengeo_api(max_tries = max_tries, verbosity = verbosity)
 
   if (!is.null(ret)) {
-    ret |>
-      httr2::resp_body_json() |>
+    data <- possibly_parse_json(ret)
+    # sometimes it gets returned with the wrong application type?
+    # and it's JSON but not marked as such - needs to be parsed as plain text
+    if (is.null(data)) {
+      data <- httr2::resp_body_string(ret) |>
+        jsonlite::fromJSON(data)
+    }
+    data |>
       purrr::pluck("objectIds") |>
       purrr::list_c()
-  } else ret
+  } else NULL
 }
 
 #' Perform an API query and handle the returned table data
-#' 
+#'
 #' @inheritParams table_data_req
 #' @inheritParams query_opengeo_api
 return_table_data <- function(
@@ -144,17 +157,21 @@ return_table_data <- function(
     possibly_query_opengeo_api(max_tries = max_tries, verbosity = verbosity)
 
   if (!is.null(ret)) {
-    ret |>
-      httr2::resp_body_json() |>
-      purrr::pluck("features") |>
-      purrr::map("attributes") |>
-      purrr::list_rbind() |>
+    # slightly more verbose than using purrr::map_df ... but safer?
+    # https://github.com/tidyverse/purrr/issues/1007#issuecomment-1373624353
+    tibble::tibble(
+      list = ret |>
+        httr2::resp_body_json() |>
+        purrr::pluck("features") |>
+        purrr::map("attributes")
+      ) |>
+      tidyr::unnest_wider(list) |>
       janitor::clean_names()
-  } else ret
+  } else NULL
 }
 
 #' Perform an API query and handle the returned spatial data
-#' 
+#'
 #' @inheritParams bounds_data_req
 #' @inheritParams query_opengeo_api
 return_bounds_data <- function(
@@ -171,5 +188,5 @@ return_bounds_data <- function(
     ret |>
       httr2::resp_body_string() |>
       sf::st_read(quiet = TRUE)
-  } else ret
+  } else NULL
 }
