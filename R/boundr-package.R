e8cc4dfc60af2c75d565b5a2e5ded5e9e3d36718
Fran Barton <fbarton@alwaysdata.net>
2023-04-30 19:14:53 +0100

    Update dependencies/imports and related docs

--- R/boundr-package.R
+++ R/boundr-package.R
@@ -3,10 +3,11 @@
 
 ## usethis namespace: start
 #' @importFrom assertthat assert_that
-#' @importFrom dplyr across all_of any_of desc if_all if_any
-#' @importFrom dplyr contains matches ends_with starts_with
+#' @importFrom dplyr desc across if_all if_any
+#' @importFrom glue glue
+#' @importFrom rlang :=
+#' @importFrom tidyselect all_of any_of ends_with starts_with
 #' @importFrom usethis ui_info
-#' @importFrom stringr str_glue
 
 ## usethis namespace: end
 NULL
