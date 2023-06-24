b13ad51c6e8441598096c729f06771428ba031af
Fran Barton <fbarton@alwaysdata.net>
2023-06-24 23:26:37 +0100

    Package documentation

--- R/boundr-package.R
+++ R/boundr-package.R
@@ -3,11 +3,12 @@
 
 ## usethis namespace: start
 #' @importFrom assertthat assert_that
-#' @importFrom dplyr desc across if_all if_any
+#' @importFrom dplyr desc across c_across if_all if_any
 #' @importFrom glue glue
 #' @importFrom rlang :=
-#' @importFrom tidyselect all_of any_of ends_with starts_with
+#' @importFrom tidyselect all_of any_of ends_with matches starts_with
 #' @importFrom usethis ui_info
+#' @importFrom utils head tail
 
 ## usethis namespace: end
 NULL
