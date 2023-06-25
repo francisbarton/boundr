cb7485cdb690d807be6f9df47e41a1edf320f472
Fran Barton <fbarton@alwaysdata.net>
2023-06-25 16:08:09 +0100

    Enable bounds and points to shortcut the lookup table process and just return spatial data

--- R/bounds.R
+++ R/bounds.R
@@ -2,82 +2,143 @@
 #'
 #' @inheritParams create_lookup_table
 #' @inheritParams spatial_data_req
-#' @param resolution character. How generalised should the boundary be, and
-#'  whether coastal boundaries should adhere to the coastline or to the full
-#'  territorial extent. BGC by default (G = Generalised (20m), C = limited to
-#'  the coastline.) F indicates Full resolution; S indicates Super-generalised
-#'  (200m); U indicates Ultra-generalised (500m) boundary resolution.
+#' @param resolution character. See argument options for available options. How
+#'  generalised should the boundary be, and whether coastal boundaries should
+#'  adhere to the coastline or to the full territorial extent. BGC by default
+#'  (G = Generalised (20m), C = limited to the coastline.) F indicates Full
+#'  resolution; S indicates Super-generalised (200m); U indicates Ultra-
+#'  generalised (500m) boundary resolution.
+#'
+#' @examples
+#' bounds("msoa", "lad", "Swansea")
+#' bounds("msoa", "lad", "Shepway", lookup_year = 2011, within_year = 2015)
+#' bounds("rgn", country_filter = "EN", resolution = "BUC")
+#' bounds("parish", "utla", "Isles of Scilly")
+#' bounds("spr")
+#' bounds("npark", within_names = "Bannau Brycheiniog")
+#'
 #'
 #' @returns an `sfc` tibble (data frame with geometry)
 #' @export
 bounds <- function(
   lookup,
-  within,
+  within = NULL,
   within_names = NULL,
   within_codes = NULL,
   return_width = c("tidy", "full", "minimal"),
   lookup_year = NULL,
   within_year = NULL,
-  country_filter = c("UK|EW|EN|WA", "UK", "EW", "EN", "WA"),
+  country_filter = c("UK|EW|EN|SC|WA", "UK", "EW", "EN", "SC", "WA"),
   resolution = c("BGC", "BSC", "BUC", "BFC", "BFE"),
   option = NULL,
-  include_welsh = FALSE,
   crs = 4326
 ) {
-  lookup <- process_aliases(lookup)
-  within <- process_aliases(within)
-  return_width <- match.arg(return_width)
+  new_lookup <- process_aliases(lookup)
   country_filter <- match.arg(country_filter)
   resolution <- match.arg(resolution)
 
-  lookup_table <- create_lookup_table(
-    lookup,
-    within,
-    within_names,
-    within_codes,
-    return_width,
-    lookup_year,
-    within_year,
-    country_filter,
-    option,
-    include_welsh)
+  if (!is.null(within)) {
+    new_within <- process_aliases(within)
+    return_width <- match.arg(return_width)
 
-  assert_that(nrow(lookup_table) > 0,
-    msg = "bounds: create_lookup_table() has returned a table with 0 rows")
+    lookup_table <- create_lookup_table(
+      new_lookup,
+      new_within,
+      within_names,
+      within_codes,
+      return_width,
+      lookup_year,
+      within_year,
+      country_filter,
+      option)
 
-  # This isn't watertight: if you have a table with eg "lad16cd" and "lad21cd"
-  # columns, it will pull the leftmost column to use for the geometry query,
-  # which may not be what you want. This should only occur when you use the
-  # 'full' return_width option.
-  # It is fairly rare that more than 1 column name will match, and it can be
-  # avoided in most cases by choosing a more appropriate 'option' parameter.
-  # A 'tidy' or 'minimal' return_width parameter will eliminate this risk.
-  geo_code_field <- lookup_table |>
-    dplyr::select(starts_with(lookup) & ends_with("cd")) |>
-    dplyr::select(1) |> # select the leftmost matching column
-    names()
+    assert_that(nrow(lookup_table) > 0,
+      msg = "bounds: create_lookup_table() has returned a table with 0 rows")
 
-  assert_that(length(geo_code_field) == 1 & !is.na(geo_code_field),
-    msg = "bounds: suitable geo_code_field not found from lookup table")
+    # This isn't watertight: if you have a table with eg "lad16cd" and "lad21cd"
+    # columns, it will pull the leftmost column to use for the geometry query,
+    # which may not be what you want. This should only occur when you use the
+    # 'full' return_width option.
+    # It is fairly rare that more than 1 column name will match, and it can be
+    # avoided in most cases by choosing a more appropriate 'option' parameter.
+    # A 'tidy' or 'minimal' return_width parameter will eliminate this risk.
+    geo_code_field <- lookup_table |>
+      dplyr::select(starts_with(new_lookup) & ends_with("cd")) |>
+      dplyr::select(1) |> # select the leftmost matching column
+      names()
 
-  area_codes <- lookup_table |>
-    dplyr::pull({{ geo_code_field }}) |>
-    batch_it(50) |> # turns out this limit is rather crucial!
-    purrr::map(\(x) paste_area_codes(var = geo_code_field, vec = x))
+    assert_that(length(geo_code_field) == 1 & !is.na(geo_code_field),
+      msg = "bounds: suitable geo_code_field not found from lookup table")
 
-  ids <- area_codes |>
-    purrr::map(\(x) return_result_ids(url = query_base_url, where = x)) |>
-    purrr::list_c()
+    area_codes <- lookup_table |>
+      dplyr::pull({{ geo_code_field }}) |>
+      batch_it(50) |> # turns out this limit is rather crucial!
+      purrr::map(\(x) paste_area_codes(var = geo_code_field, vec = x))
+
+    query_base_url <- pull_bounds_query_url(geo_code_field, new_lookup, resolution)
+
+    ids <- area_codes |>
+      purrr::map(\(x) return_result_ids(url = query_base_url, where = x)) |>
+      purrr::list_c()
+  } else {
+    lookup_table <- NULL
+
+    bounds_query_info <- return_spatial_query_info(
+      new_lookup,
+      lookup_year,
+      option
+    )
+    query_base_url <- bounds_query_info[["query_url"]]
+
+    lookup_code_field <- bounds_query_info[["lookup_field"]]
+    lookup_name_field <- sub("cd$", "nm", lookup_code_field)
+
+
+    if (is.null(within_names) & is.null(within_codes)) {
+      within_string <- "1=1"
+    } else if (!is.null(within_names)) {
+      within_string <- lookup_name_field |>
+        paste0(
+          " IN (",
+          stringr::str_flatten(
+            paste0("'", within_names, "'"),
+            collapse = ","),
+          ")"
+        ) |>
+        head(length(within_names)) |>
+        stringr::str_flatten(collapse = " OR ")
+    } else if (!is.null(within_codes)) {
+      within_string <- lookup_code_field |>
+        paste0(
+          " IN (",
+          stringr::str_flatten(
+            paste0("'", within_codes, "'"),
+            collapse = ","),
+          ")"
+        ) |>
+        head(length(within_codes)) |>
+        stringr::str_flatten(collapse = " OR ")
+    } else {
+      within_string <- NULL
+    }
+
+    assert_that(!is.null(within_string),
+                msg = "bounds: The within_names or within_codes arguments have not resulted in a valid query string.")
+
+    ids <- query_base_url |>
+      return_result_ids(where = within_string) |>
+      unique()
+  }
+
 
   assert_that(is.vector(ids) & !is.list(ids) & length(ids),
-    msg = "bounds: return_result_ids() has not returned a vector of IDs.")
+      msg = "bounds: return_result_ids() has not returned a vector of IDs.")
 
-  query_base_url <- pull_bounds_query_url(geo_code_field, lookup, resolution)
   bounds_data <- ids |>
-    purrr::map(\(x) return_bounds_data(x, query_base_url, crs))
+    purrr::map(\(x) return_spatial_data(x, query_base_url, crs))
 
   assert_that(is.list(bounds_data) & length(bounds_data),
-    msg = "bounds: return_bounds_data() has not returned a list of length > 0")
+    msg = "bounds: return_spatial_data() has not returned a list of length > 0")
 
   bounds_data_df <- bounds_data |>
     dplyr::bind_rows() |>
@@ -91,16 +152,20 @@ bounds <- function(
     msg = "bounds: the data frame bounds_data_df does not have 'sf' class")
 
 
-  join_vars <- intersect(names(lookup_table), names(bounds_data_df))
+  if (!is.null(lookup_table)) {
+    join_vars <- intersect(names(lookup_table), names(bounds_data_df))
 
-  if (return_width %in% c("tidy", "minimal")) {
+    if (return_width %in% c("tidy", "minimal")) {
+      bounds_data_df <- bounds_data_df |>
+        dplyr::select(all_of(c(join_vars, "geometry")))
+    }
+
     bounds_data_df <- bounds_data_df |>
-      dplyr::select(all_of(c(join_vars, "geometry")))
+      dplyr::left_join(lookup_table, join_vars) |>
+      dplyr::relocate(names(lookup_table))
   }
 
   bounds_data_df |>
-    dplyr::left_join(lookup_table, join_vars) |>
-    dplyr::relocate(names(lookup_table)) |>
     dplyr::select(!any_of(c("fid", "objectid", "global_id"))) |>
     janitor::remove_empty("cols") |>
     dplyr::distinct()
@@ -133,7 +198,7 @@ pull_bounds_query_url <- function(field, lookup, resolution) {
   results |>
     # We assume any row of results will give the desired geo data. So take #1.
     dplyr::slice(1) |>
-    dplyr::pull(all_of("service_url"))
+    dplyr::pull("service_url")
 }
 
 
--- R/points.R
+++ R/points.R
@@ -7,36 +7,38 @@
 #' @export
 points <- function(
     lookup,
-    within,
+    within = NULL,
     within_names = NULL,
     within_codes = NULL,
     return_width = c("tidy", "full", "minimal"),
     lookup_year = NULL,
     within_year = NULL,
-    country_filter = c("UK|EW|EN|WA", "UK", "EW", "EN", "WA"),
+    country_filter = c("UK|EW|EN|SC|WA", "UK", "EW", "EN", "SC", "WA"),
+    resolution = c("BGC", "BSC", "BUC", "BFC", "BFE"),
     option = NULL,
-    include_welsh = FALSE,
     crs = 4326
 ) {
   lookup <- process_aliases(lookup)
-  within <- process_aliases(within)
-  return_width <- match.arg(return_width)
   country_filter <- match.arg(country_filter)
+  resolution <- match.arg(resolution)
 
-  lookup_table <- create_lookup_table(
-    lookup,
-    within,
-    within_names,
-    within_codes,
-    return_width,
-    lookup_year,
-    within_year,
-    country_filter,
-    option,
-    include_welsh)
+  if (!is.null(within)) {
+    within <- process_aliases(within)
+    return_width <- match.arg(return_width)
 
-  assert_that(nrow(lookup_table) > 0,
-              msg = "points: create_lookup_table() has returned a table with 0 rows")
+    lookup_table <- create_lookup_table(
+      lookup,
+      within,
+      within_names,
+      within_codes,
+      return_width,
+      lookup_year,
+      within_year,
+      country_filter,
+      option)
+
+    assert_that(nrow(lookup_table) > 0,
+                msg = "bounds: create_lookup_table() has returned a table with 0 rows")
 
   # This isn't watertight: if you have a table with eg "lad16cd" and "lad21cd"
   # columns, it will pull the leftmost column to use for the geometry query,
@@ -64,10 +66,58 @@ points <- function(
   ids <- area_codes |>
     purrr::map(\(x) return_result_ids(url = query_base_url, where = x)) |>
     purrr::list_c()
+  } else {
+    lookup_table <- NULL
+
+    points_query_info <- return_bounds_query_info(
+      lookup,
+      lookup_year,
+      option
+    )
+    query_base_url <- points_query_info[["query_url"]]
+
+    lookup_code_field <- points_query_info[["lookup_field"]]
+    lookup_name_field <- sub("cd$", "nm", lookup_code_field)
+
+
+    if (is.null(within_names) & is.null(within_codes)) {
+      within_string <- "1=1"
+    } else if (!is.null(within_names)) {
+      within_string <- lookup_name_field |>
+        paste0(
+          " IN (",
+          stringr::str_flatten(
+            paste0("'", within_names, "'"),
+            collapse = ","),
+          ")"
+        ) |>
+        head(length(within_names)) |>
+        stringr::str_flatten(collapse = " OR ")
+    } else if (!is.null(within_codes)) {
+      within_string <- lookup_code_field |>
+        paste0(
+          " IN (",
+          stringr::str_flatten(
+            paste0("'", within_codes, "'"),
+            collapse = ","),
+          ")"
+        ) |>
+        head(length(within_codes)) |>
+        stringr::str_flatten(collapse = " OR ")
+    } else {
+      within_string <- NULL
+    }
+
+    assert_that(!is.null(within_string),
+                msg = "bounds: The within_names or within_codes arguments have not resulted in a valid query string.")
+
+    ids <- query_base_url |>
+      return_result_ids(where = within_string) |>
+      unique()
+  }
 
   assert_that(is.vector(ids) & !is.list(ids) & length(ids),
               msg = "points: return_result_ids() has not returned a vector of IDs.")
-
 
   points_data <- ids |>
     purrr::map(\(x) return_spatial_data(x, query_base_url, crs))
@@ -86,17 +136,20 @@ points <- function(
   assert_that(inherits(points_data_df, "sf"),
               msg = "points: the data frame bounds_data_df does not have 'sf' class")
 
+  if (!is.null(lookup_table)) {
+    join_vars <- intersect(names(lookup_table), names(points_data_df))
 
-  join_vars <- intersect(names(lookup_table), names(points_data_df))
+    if (return_width %in% c("tidy", "minimal")) {
+      points_data_df <- points_data_df |>
+        dplyr::select(all_of(c(join_vars, "geometry")))
+    }
 
-  if (return_width %in% c("tidy", "minimal")) {
     points_data_df <- points_data_df |>
-      dplyr::select(all_of(c(join_vars, "geometry")))
+      dplyr::left_join(lookup_table, join_vars) |>
+      dplyr::relocate(names(lookup_table))
   }
 
   points_data_df |>
-    dplyr::left_join(lookup_table, join_vars) |>
-    dplyr::relocate(names(lookup_table)) |>
     dplyr::select(!any_of(c("fid", "objectid", "global_id"))) |>
     janitor::remove_empty("cols") |>
     dplyr::distinct()
@@ -121,10 +174,10 @@ pull_centroid_query_url <- function(field, lookup) {
   assert_that(nrow(results) > 0,
               msg = paste0(
                 "No centroid data was found for the parameters supplied. ",
-                "Try a different year or a different resolution?"))
+                "Try a different year?"))
 
   results |>
     # We assume any row of results will give the desired geo data. So take #1.
     dplyr::slice(1) |>
-    dplyr::pull(all_of("service_url"))
+    dplyr::pull("service_url")
 }
