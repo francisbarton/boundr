0768a129a3ed075e5f3e4f75a4909b093897e05a
Fran Barton <francis.barton@nhs.net>
2024-08-18 23:35:32 +0100

    Pull out common procedure for lookups

--- R/common_lookup.R
+++ R/common_lookup.R
@@ -1,3 +1,53 @@
+#' Common procedure for joinable and not-necessarily-joinable lookup()s
+#' 
+#' @param joinable logical. Whether this query is joinable (`TRUE`) or
+#'  forms part of a geospatial query (`FALSE`). In the latter case, this
+#'  function will try to return a lookup table that contains the appropriate
+#'  field for spatial data (that is, boundaries or centroids) to be joined onto.
+#'  For `{boundr}`'s internal use only, users should ignore this.
+#' @keywords internal
+common_lookup <- function(
+    lookup,
+    within_level = NULL,
+    within_names = NULL,
+    within_codes = NULL,
+    lookup_year = NULL,
+    within_year = NULL,
+    opts = boundr_options(),
+    joinable = FALSE) {
+  lookup <- tolower(lookup)
+  resolution <- opts[["rs"]]
+  return_width <- opts[["rw"]]
+  query_option <- opts[["opt"]]
+
+  if (is.null(within_level)) {
+    query_info <- return_narrow_table_info(lookup, lookup_year)
+  } else {
+    query_info <- return_lookup_table_info(
+      lookup,
+      within_level,
+      lookup_year,
+      within_year,
+      joinable
+    )
+  }
+
+  query_data <- query_info |>
+    process_query_info(within_names, within_codes, return_width, query_option)
+  tbl <- process_lookup_query_data(query_data) |>
+    dplyr::bind_rows() |>
+    dplyr::select(!any_of(c("object_id", "global_id", "chgind"))) |>
+    dplyr::distinct()
+
+  if (lookup == "msoa") tbl <- add_msoa_names(tbl)
+  if (return_width != "full") tbl <- remove_nmw(tbl)
+
+  tbl |>
+    dplyr::distinct() |>
+    janitor::remove_empty("cols")
+}
+
+
 # Helper functions
 
 #' If our `lookup` is MSOA, add the House of Commons Library MSOA Names -
