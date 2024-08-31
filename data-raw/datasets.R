1a8eb418bf755641a5d695718db5bec0a9704cb4
Fran Barton <francis.barton@nhs.net>
2024-08-31 01:59:07 +0100

    Updata internal data creation script

--- data-raw/datasets.R
+++ data-raw/datasets.R
@@ -17,6 +17,8 @@ hocl_msoa21_names <- paste0(
 
 # Open Geography schema build -------------
 
+devtools::load_all()
+
 build_schema <- function() {
 
   # Helper functions
@@ -48,10 +50,7 @@ build_schema <- function() {
   }
 
   # Data pipeline
-  api_base_url <-
-    "https://services1.arcgis.com/ESMARspQHYMw9BZ9/ArcGIS/rest/services"
-
-  api_services_data <- api_base_url |>
+  api_services_data <- base_url() |>
     opengeo_api_req(append = "") |>
     query_opengeo_api() |>
     httr2::resp_body_json() |>
@@ -71,10 +70,10 @@ build_schema <- function() {
   if (length(fails)) {
     info <- api_services_data[["name"]][fails] |> # nolint
       cli::cli_vec(c(`vec-trunc` = 3))
-    cli_alert_info(
+    cli_alert_info(c(
       "{length(fails)} services did not successfully return data this time. ",
       "Examples: {info}"
-    )
+    ))
   }
 
   service_urls <- api_services_data[["service_url"]][-fails]
@@ -84,75 +83,92 @@ build_schema <- function() {
     purrr::compact() |>
     purrr::map(httr2::resp_body_json) |>
     purrr::map2(
-      service_urls, pluck_api_data, .progress = "Retrieving schema data"
+      service_urls, pluck_api_data,
+      .progress = "Retrieving schema data"
     ) |>
     purrr::list_rbind() |>
     dplyr::filter(!if_all(ends_with("cd"), is.na)) |>
     janitor::remove_empty("cols") |>
     dplyr::select("service_name":"has_geometry", ends_with("cd")) |>
-    dplyr::mutate(across("service_name", \(x) stringr::str_replace_all(x, c(
-      "^Built_Up_Area" = "BUA",
-      "^Cancer_Alliances" = "CAL",
-      "^Clinical_Commissioning_Groups" = "CCG",
-      "^Combined_Authorities" = "CAUTH",
-      "^Community_Safety_Partnerships" = "CSP",
-      "^Counties" = "CTY",
-      "^Counties_and_Unitary_Authorities" = "CTYUA",
-      "^Countries" = "CTRY",
-      "^CUA" = "CTYUA",
-      "^European_Electoral_Regions" = "EER",
-      "^Fire_and_Rescue_Authorities" = "FRA",
-      "^Index_of_Multiple_Deprivation" = "IMD",
-      "^London_Assembly_Constituencies" = "LAC",
-      "^Local_Authority_Districts" = "LAD",
-      "^Local_Enterprise_Partnerships" = "LEP",
-      "^Local_Health_Boards" = "LHB",
-      "^Local_Planning_Authorities" = "LPA",
-      "^Local_Resilience_Forums" = "LRF",
-      "^Metropolitan_Counties" = "MCTY",
-      "^National_Assembly_Economic_Regions" = "NAER",
-      "^NHS_Commissioning_RGNs" = "NHSCR",
-      "^NHS_England_Regions" = "NHSER",
-      "^National_Parks" = "NPAR",
-      "^Output_Area" = "OA",
-      "^Parishes" = "PAR",
-      "^Parishes_and_Non_Civil_Parished_Areas" = "PARNCP",
-      "^Police_Force_Areas" = "PFA",
-      "^Public_Health_England_Centres" = "PHEC",
-      "^Public_Health_England_RGNs" = "PHEREG",
-      "^Regions" = "RGN",
-      "^Registration_Districts" = "REGD",
-      "^Strategic_Clinical_Networks" = "SCN",
-      "^Sustainability_and_Transformation_Partnerships" = "STP",
-      "^Travel_to_Work_Areas" = "TTWA",
-      "^Wards" = "WD",
-      "^Westminster_Parliamentary_Constituencies" = "PCON",
-      "^Workplace_Zones" = "WZ",
-      "Local_Authority_District" = "LAD",
-      "Region" = "RGN",
-      "Lookup" = "LU",
-      "_Wales$" = "_WA",
-      "Ultra_Generalised_Clipped_Boundaries" = "UGCB",
-      "Ultra_Generalised_Boundaries" = "UGCB",
-      "Generalised_Clipped_Boundaries" = "GCB",
-      "Generalised_Clipped" = "GCB",
-      "Generalised_Boundaries" = "GCB",
-      "Full_Clipped_Boundaries" = "FCB",
-      "Full_Extent_Boundaries" = "FEB",
-      "Full_Clipped" = "FCB",
-      "Full_Extent" = "FEB",
-      "Full_extent" = "FEB",
-      "_UGB" = "UGCB",
-      "_FECB" = "FEB",
-      "_FRCB" = "FCB"
-    )))) |>
+    dplyr::mutate(
+      across("service_name", \(x) gsub("\\sv", "_v", x)), # do this first
+      across("service_name", \(x) gsub("__", "_", x)),
+      across("service_name", \(x) {
+        stringr::str_replace_all(x, c(
+          "^Built_Up_Area" = "BUA",
+          "^Cancer_Alliances" = "CAL",
+          "^Clinical_Commissioning_Groups" = "CCG",
+          "^Combined_Authorities" = "CAUTH",
+          "^Community_Safety_Partnerships" = "CSP",
+          "^Counties" = "CTY",
+          "^Counties_and_Unitary_Authorities" = "CTYUA",
+          "^Countries" = "CTRY",
+          "^Country" = "CTRY",
+          "^CUA" = "CTYUA",
+          "^European_Electoral_Regions" = "EER",
+          "^Fire_and_Rescue_Authorities" = "FRA",
+          "^Index_of_Multiple_Deprivation" = "IMD",
+          "^London_Assembly_Constituencies" = "LAC",
+          "^Local_Authority_Districts" = "LAD",
+          "^Local_Enterprise_Partnerships" = "LEP",
+          "^Local_Health_Boards" = "LHB",
+          "^Local_Planning_Authorities" = "LPA",
+          "^Local_Resilience_Forums" = "LRF",
+          "^Metropolitan_Counties" = "MCTY",
+          "^National_Assembly_Economic_Regions" = "NAER",
+          "^NHS_Commissioning_RGNs" = "NHSCR",
+          "^NHS_England_Regions" = "NHSER",
+          "^National_Parks" = "NPARK",
+          "^Output_Area" = "OA",
+          "^Parishes" = "PAR",
+          "^Parishes_and_Non_Civil_Parished_Areas" = "PARNCP",
+          "^Police_Force_Areas" = "PFA",
+          "^Public_Health_England_Centres" = "PHEC",
+          "^Public_Health_England_RGNs" = "PHEREG",
+          "^Regions" = "RGN",
+          "^Registration_Districts" = "REGD",
+          "^Strategic_Clinical_Networks" = "SCN",
+          "^Sustainability_and_Transformation_Partnerships" = "STP",
+          "^Travel_to_Work_Areas" = "TTWA",
+          "^Wards" = "WD",
+          "_Ward_" = "_WD_",
+          "^Westminster_Parliamentary_Constituencies" = "PCON",
+          "^Workplace_Zones" = "WZ",
+          "Local_Authority_District" = "LAD",
+          "Lower_Tier_Local_Authority" = "LTLA",
+          "Upper_Tier_Local_Authority" = "UTLA",
+          "Region" = "RGN",
+          "Lookup" = "LU",
+          "_Wales$" = "_WA",
+          "England" = "EN",
+          "Great_Britain" = "GB",
+          "Scotland" = "SC",
+          "Sub_ICB_Locations" = "SICBL",
+          "Ultra_Generalised_Clipped_Boundaries" = "UGCB",
+          "Ultra_Generalised_Boundaries" = "UGCB",
+          "Generalised_Clipped_Boundaries" = "GCB",
+          "Generalised_Clipped" = "GCB",
+          "Generalised_Boundaries" = "GCB",
+          "Full_Clipped_Boundaries" = "FCB",
+          "Full_Extent_Boundaries" = "FEB",
+          "Full_Clipped" = "FCB",
+          "Full_Extent" = "FEB",
+          "Full_extent" = "FEB",
+          "_UGB" = "UGCB",
+          "_FECB" = "FEB",
+          "_FRCB" = "FCB"
+        ))
+      })
+    ) |>
     dplyr::arrange(desc(pick("data_edit_date")))
 }
 
 opengeo_schema <- build_schema()
 
-usethis::use_data(hocl_msoa11_names, hocl_msoa21_names, opengeo_schema,
-  overwrite = TRUE,
+usethis::use_data(
+  hocl_msoa11_names,
+  hocl_msoa21_names,
+  opengeo_schema,
   internal = TRUE,
-  compress = "bzip2"
+  overwrite = TRUE
 )
