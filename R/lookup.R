4b1fd9b1c86bee7a1214076d1489ef59a2bb412e
Fran Barton <francis.barton@nhs.net>
2024-08-31 01:59:06 +0100

    Move crate_lookup_table() to the neater lookup()

---
+++ R/lookup.R
@@ -0,0 +1,46 @@
+#' Create a lookup table by querying the ONS OpenGeography API
+#'
+#' @param lookup character. Lower level area code eg "lsoa", "wd", "lad".
+#' @param within_level character. Higher level area code eg "lad", "cty", "icb".
+#'  (That is, higher (conceptually) than `lookup`; aka larger than `lookup`!).
+#'  If not supplied, this uses the default `NULL`, and just data for `lookup`
+#'  will be returned. In this case, `within_names` and `within_codes` (if
+#'  supplied) will instead be taken to refer to, and used to filter at, the
+#'  `lookup` level.
+#' @param within_names,within_codes character. In order to restrict data
+#'  returned to a specific area, either `within_names` or `within_codes` must
+#'  be provided. Otherwise all available boundaries at that level will be
+#'  retrieved. Use place names eg "Essex" to restrict to a certain
+#'  geographical area. Or use ONS area codes eg "W02000103" likewise (this is
+#'  useful with wards, where there are many that share identical names).
+#'  To use this argument to filter `within_level`, `the `within_level` must be
+#'  specified! Otherwise `{boundr}` will apply them to `lookup` instead.
+#'  See examples.
+#'  Vectors of multiple names or multiple codes can be supplied.
+#'  If you supply both `within_names` and `within_codes`, only `within_names`
+#'  will be used; `within_codes` will be ignored.
+#' @param lookup_year numeric or character. A specific year for data relating
+#'  to `lookup`, if you need it. Defaults to `NULL`, which then aims to return
+#'  data for the most recent year available. Provide a full 4 digit number.
+#' @param within_year numeric or character. A specific year for data relating
+#'  to `within_level`, if you need it. Defaults to `NULL`, which then aims to
+#'  return data for the most recent year available.  Provide as YYYY.
+#' @inheritParams bounds
+#'
+#' @examples
+#' lookup("msoa", "lad", "Swindon")
+#' lookup("wd", "sener", opts = opts(return_width = "full"))
+#'
+#' @returns A tibble
+#' @export
+lookup <- function(
+    lookup,
+    within_level = NULL,
+    within_names = NULL,
+    within_codes = NULL,
+    lookup_year = NULL,
+    within_year = NULL,
+    opts = boundr_options()) {
+  curr_args <- rlang::call_args(rlang::current_call())
+  common_lookup(!!!curr_args)
+}
