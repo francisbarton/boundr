2bbbfdfbef0c70d0fb30344641f27e0ffd65d9af
Fran Barton <francis.barton@nhs.net>
2024-08-31 02:00:38 +0100

    Update overall tests and examples

---
+++ tests/testthat/test-lookup.R
@@ -0,0 +1,13 @@
+test_that("overall lookup() test", {
+  expect_no_error(lookup("spr"))
+  expect_no_error(lookup("npark"))
+  expect_no_error(lookup("spc", "spr"))
+  expect_no_error(lookup("npark", within_names = "Exmoor"))
+  expect_no_error(lookup("msoa", "lad", "Stroud", opts = opts(return_width = "full")))
+  expect_no_error(lookup("lad", "ctry", "Wales", lookup_year = 2022))
+  expect_no_error(lookup("ltla", "iol", "inner london"))
+  expect_no_error(lookup("itl2", "itl1", within_codes = "TLH"))
+  expect_no_error(lookup("icb", "nhser", "South East")) # check nrow() accurate
+  expect_no_error(lookup("sener", within_names = "North Wales"))
+  expect_error(lookup("sener", within_names = "Gogledd Cymru")) # to_fix
+})
